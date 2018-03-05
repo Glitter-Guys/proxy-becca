46b56e4f2d384e191cd1206fd67a8082c5d3ca66
Becca Stanger <beccastanger@gmail.com>
2018-03-05 15:13:16 -0800

    Add additional initial proxy files

--- proxy/server.js
+++ proxy/server.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const app = express();
+const path = require('path');
+const port = '8888';
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, '127.0.0.1', () => console.log(`Proxy listening on port ${port}`));
