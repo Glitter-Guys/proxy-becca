4121c1ae795325f129d2718bffec84ec325c6d61
Becca Stanger <beccastanger@gmail.com>
2018-03-05 17:20:29 -0800

    Get where-when server running in proxy

--- proxy/server.js
+++ proxy/server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const port = '8888';
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use('/event/:eventid', express.static(path.join(__dirname, './public')));
 
 app.listen(port, '127.0.0.1', () => console.log(`Proxy listening on port ${port}`));
